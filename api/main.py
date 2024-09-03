7026a0182b03c72ec9839ae9fa7d85e234bcc2f2
Arturo Huari <e_2020100535E@uncp.edu.pe>
2024-09-03 10:36:39 -0500

    CORS

--- api/main.py
+++ api/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
 from io import BytesIO
 
 app = FastAPI()
+
+# Configurar las políticas de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Reemplaza esto con el origen de tu front-end
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos los métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permite todos los encabezados
+)
 
 @app.get("/")
 async def hello_world():
@@ -12,7 +22,7 @@ async def hello_world():
 async def upload_file(file: UploadFile = File(...)):
     try:
         contents = await file.read()
-        df = pd.read_excel(BytesIO(contents))
+        df = pd.read_excel(BytesIO(contents), skiprows=5)
         
         # Aquí se ejecuta el proceso ETL usando el DataFrame 'df'
         # Ejemplo de conexión y carga de datos a PostgreSQL
@@ -30,11 +40,10 @@ async def upload_file(file: UploadFile = File(...)):
         # conn.commit()
         # cursor.close()
         # conn.close()
-
         return {
             "filename": file.filename, 
             "message": "Archivo procesado y cargado con éxito",
-            "info": str(df.info())
+            "info": str(type(df))
             }
 
     except Exception as e:
